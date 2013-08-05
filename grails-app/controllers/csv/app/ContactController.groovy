8fe3a3f4260ab16e9165f11f6222bceb5db1474d
Livien Yin <LivienYin@gmail.com>
2013-08-04 20:33:32 -0700

    File parser skips header row.

--- grails-app/controllers/csv/app/ContactController.groovy
+++ grails-app/controllers/csv/app/ContactController.groovy
@@ -116,23 +116,24 @@ class ContactController {
         def tempFile = request.getFile('file')
         tempFile.transferTo(new File('/Users/livienyin/Desktop/temp/tempFile.csv'))
 
-        new File('/Users/livienyin/Desktop/temp/tempFile.csv').splitEachLine(',') { row ->
-
-            def contact = Contact.findByEmail(row[0]) ?: new Contact(
-                email: row[0],
-                firstName: row[1],
-                lastName: row[2],
-                prefix: row[3],
-                phone: row[4],
-                fax: row[5],
-                title: row[6])
-            def company = Company.findByName(row[7]) ?: new Company(name: row[7])
-            company.save(flush: true)
-            company.addToContacts(contact)
-            contact.save(flush: true)
-
-            println contact
-
+        File csvFile = new File('/Users/livienyin/Desktop/temp/tempFile.csv')
+        def rowNum = 0
+        csvFile.splitEachLine(',') { row ->
+            if (rowNum > 0) {
+                def contact = Contact.findByEmail(row[0]) ?: new Contact(
+                    email: row[0],
+                    firstName: row[1],
+                    lastName: row[2],
+                    prefix: row[3],
+                    phone: row[4],
+                    fax: row[5],
+                    title: row[6])
+                def company = Company.findByName(row[7]) ?: new Company(name: row[7])
+                company.save(flush: true)
+                company.addToContacts(contact)
+                contact.save(flush: true)
+            }
+            rowNum++
         }
 
         render(view: "list", model: [contactInstanceList: Contact.list(params), contactInstanceTotal: Contact.count()])
