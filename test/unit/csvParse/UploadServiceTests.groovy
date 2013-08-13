671cc6c748f7fe66a92c78b32165dbed55019659
Livien Yin <livienyin@gmail.com>
2013-08-12 17:27:03 -0700

    Testing for csv parser service. Parser should be re-written.

--- test/unit/csvParse/UploadServiceTests.groovy
+++ test/unit/csvParse/UploadServiceTests.groovy
@@ -1,17 +1,44 @@
 package csvParse
 
-
-
+import csv.app.Company
+import csv.app.Contact
 import grails.test.mixin.*
+import csvParse.UploadService
 import org.junit.*
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(UploadService)
+@Mock([Contact, Company])
 class UploadServiceTests {
 
-    void testSomething() {
-        fail "Implement me"
+    def uploadService = new UploadService()
+
+    void testParserSavesContacts() {
+        File csvFile = new File('/tmp/tempFile.csv')
+        uploadService.parseCsvFile(csvFile)
+        assert Contact.count() == 8
     }
+
+    void testParserSavesCompanies() {
+        File csvFile = new File('/tmp/tempFile.csv')
+        uploadService.parseCsvFile(csvFile)
+        assert Company.count() == 7
+    }
+
+    void testParserAssignsCorrectAttributes() {
+        File csvFile = new File('/tmp/tempFile.csv')
+        uploadService.parseCsvFile(csvFile)
+        def firstContact = Contact.first()
+        assert firstContact.firstName == "Jonas Sultani"
+        assert firstContact.lastName == null
+        assert firstContact.email == "jonas.sultani@flintstones.com"
+        assert firstContact.prefix == "Mr"
+        assert firstContact.phone == "+49 6245 99555"
+        assert firstContact.fax == "+49 6245 95535"
+        assert firstContact.title == "Consultant"
+        assert firstContact.company == Company.first()
+    }
+
 }
